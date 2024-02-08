6023dc983c0ea4f09c4f31482f0349d4c2f72e70
Jana Šnirychová <jana.snirychova@artin.cz>
2024-02-08 18:24:46 +0100

    Add test for fizzBuzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -19,5 +19,23 @@ describe("max", () => {
 
   it("should return the first argument if arguments are equal", () => {
     expect(max(2, 2)).toBe(2);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if arg is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if arg is divisible by 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+
+  it("should return Buzz if arg is divisible by 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
+
+  it("should return arg as string if arg is not divisible by 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe("4");
   });
 });
